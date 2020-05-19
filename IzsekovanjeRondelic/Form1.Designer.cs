37fa32b3bec1a206e1e8de084ad80fa551dedd10
jurij.valent <jurij.valent@talum.si>
2020-05-19 09:33:21 +0200

    Dodani razredi za delo z EF

--- IzsekovanjeRondelic/Form1.Designer.cs
+++ IzsekovanjeRondelic/Form1.Designer.cs
@@ -28,34 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.labLenght = new System.Windows.Forms.Label();
+            this.labWidth = new System.Windows.Forms.Label();
+            this.labRdious = new System.Windows.Forms.Label();
+            this.labSpace = new System.Windows.Forms.Label();
+            this.lenghtTxt = new System.Windows.Forms.TextBox();
+            this.widthTxt = new System.Windows.Forms.TextBox();
+            this.radiousTxt = new System.Windows.Forms.TextBox();
+            this.spaceTxt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labResult = new System.Windows.Forms.Label();
+            this.resetTextBtn = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rezultat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dolzina = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.width = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Polmer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Razmik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // calculateBtn
             // 
-            this.button1.Location = new System.Drawing.Point(71, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 51);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.calculateBtn.Location = new System.Drawing.Point(157, 231);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(107, 34);
+            this.calculateBtn.TabIndex = 0;
+            this.calculateBtn.Text = "Izračunaj";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.buttonCalculate);
+            // 
+            // labLenght
+            // 
+            this.labLenght.AutoSize = true;
+            this.labLenght.Location = new System.Drawing.Point(68, 82);
+            this.labLenght.Name = "labLenght";
+            this.labLenght.Size = new System.Drawing.Size(55, 17);
+            this.labLenght.TabIndex = 1;
+            this.labLenght.Text = "Dolžina";
+            // 
+            // labWidth
+            // 
+            this.labWidth.AutoSize = true;
+            this.labWidth.Location = new System.Drawing.Point(68, 115);
+            this.labWidth.Name = "labWidth";
+            this.labWidth.Size = new System.Drawing.Size(44, 17);
+            this.labWidth.TabIndex = 2;
+            this.labWidth.Text = "Širina";
+            // 
+            // labRdious
+            // 
+            this.labRdious.AutoSize = true;
+            this.labRdious.Location = new System.Drawing.Point(68, 154);
+            this.labRdious.Name = "labRdious";
+            this.labRdious.Size = new System.Drawing.Size(52, 17);
+            this.labRdious.TabIndex = 3;
+            this.labRdious.Text = "Polmer";
+            // 
+            // labSpace
+            // 
+            this.labSpace.AutoSize = true;
+            this.labSpace.Location = new System.Drawing.Point(68, 191);
+            this.labSpace.Name = "labSpace";
+            this.labSpace.Size = new System.Drawing.Size(54, 17);
+            this.labSpace.TabIndex = 4;
+            this.labSpace.Text = "Razmik";
+            // 
+            // lenghtTxt
+            // 
+            this.lenghtTxt.Location = new System.Drawing.Point(147, 81);
+            this.lenghtTxt.Name = "lenghtTxt";
+            this.lenghtTxt.Size = new System.Drawing.Size(117, 22);
+            this.lenghtTxt.TabIndex = 5;
+            // 
+            // widthTxt
+            // 
+            this.widthTxt.Location = new System.Drawing.Point(147, 115);
+            this.widthTxt.Name = "widthTxt";
+            this.widthTxt.Size = new System.Drawing.Size(117, 22);
+            this.widthTxt.TabIndex = 5;
+            // 
+            // radiousTxt
+            // 
+            this.radiousTxt.Location = new System.Drawing.Point(147, 154);
+            this.radiousTxt.Name = "radiousTxt";
+            this.radiousTxt.Size = new System.Drawing.Size(117, 22);
+            this.radiousTxt.TabIndex = 5;
+            // 
+            // spaceTxt
+            // 
+            this.spaceTxt.Location = new System.Drawing.Point(147, 191);
+            this.spaceTxt.Name = "spaceTxt";
+            this.spaceTxt.Size = new System.Drawing.Size(117, 22);
+            this.spaceTxt.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Rezultat,
+            this.dolzina,
+            this.width,
+            this.Polmer,
+            this.Razmik});
+            this.dataGridView1.Location = new System.Drawing.Point(312, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(616, 341);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // labResult
+            // 
+            this.labResult.AutoSize = true;
+            this.labResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labResult.Location = new System.Drawing.Point(106, 317);
+            this.labResult.Name = "labResult";
+            this.labResult.Size = new System.Drawing.Size(0, 55);
+            this.labResult.TabIndex = 7;
+            // 
+            // resetTextBtn
+            // 
+            this.resetTextBtn.Location = new System.Drawing.Point(46, 231);
+            this.resetTextBtn.Name = "resetTextBtn";
+            this.resetTextBtn.Size = new System.Drawing.Size(95, 34);
+            this.resetTextBtn.TabIndex = 8;
+            this.resetTextBtn.Text = "Počisti";
+            this.resetTextBtn.UseVisualStyleBackColor = true;
+            this.resetTextBtn.Click += new System.EventHandler(this.resetTextBtn_Click);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "id";
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            this.Id.Width = 50;
+            // 
+            // Rezultat
+            // 
+            this.Rezultat.DataPropertyName = "result";
+            this.Rezultat.HeaderText = "Rezultat";
+            this.Rezultat.Name = "Rezultat";
+            this.Rezultat.Width = 70;
+            // 
+            // dolzina
+            // 
+            this.dolzina.DataPropertyName = "lenght";
+            this.dolzina.HeaderText = "Dolžina";
+            this.dolzina.Name = "dolzina";
+            this.dolzina.Width = 70;
+            // 
+            // width
+            // 
+            this.width.DataPropertyName = "width";
+            this.width.HeaderText = "Širina";
+            this.width.Name = "width";
+            // 
+            // Polmer
+            // 
+            this.Polmer.DataPropertyName = "radious";
+            this.Polmer.HeaderText = "Polmer";
+            this.Polmer.Name = "Polmer";
+            this.Polmer.Width = 70;
+            // 
+            // Razmik
+            // 
+            this.Razmik.DataPropertyName = "space";
+            this.Razmik.HeaderText = "Razmik";
+            this.Razmik.Name = "Razmik";
+            this.Razmik.Width = 70;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.resetTextBtn);
+            this.Controls.Add(this.labResult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.spaceTxt);
+            this.Controls.Add(this.radiousTxt);
+            this.Controls.Add(this.widthTxt);
+            this.Controls.Add(this.lenghtTxt);
+            this.Controls.Add(this.labSpace);
+            this.Controls.Add(this.labRdious);
+            this.Controls.Add(this.labWidth);
+            this.Controls.Add(this.labLenght);
+            this.Controls.Add(this.calculateBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label labLenght;
+        private System.Windows.Forms.Label labWidth;
+        private System.Windows.Forms.Label labRdious;
+        private System.Windows.Forms.Label labSpace;
+        private System.Windows.Forms.TextBox lenghtTxt;
+        private System.Windows.Forms.TextBox widthTxt;
+        private System.Windows.Forms.TextBox radiousTxt;
+        private System.Windows.Forms.TextBox spaceTxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labResult;
+        private System.Windows.Forms.Button resetTextBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rezultat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dolzina;
+        private System.Windows.Forms.DataGridViewTextBoxColumn width;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Polmer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Razmik;
     }
 }
 
--- IzsekovanjeRondelic/Form1.cs
+++ IzsekovanjeRondelic/Form1.cs
@@ -1,29 +1,63 @@
 ﻿using CircleInRectangleLib;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IzsekovanjeRondelic
 {
     public partial class Form1 : Form
     {
+        Slug slugObject = new Slug();
+        CircleInRectangle cir = new CircleInRectangle();
         double result = 0;
         public Form1()
         {
             InitializeComponent();
+            getSlugsList();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonCalculate(object sender, EventArgs e)
         {
-            CircleInRectangle cir = new CircleInRectangle();
+
             result = cir.numberOfSlugs(257, 157, 1.7, 1);
-            Console.WriteLine(result);
+
+            slugObject.lenght = 257;
+            slugObject.width = 157;
+            slugObject.radious = 1;
+            slugObject.space = 1;
+            slugObject.result = 3300;
+
+            labResult.Text = slugObject.result.ToString();
+            Console.WriteLine(slugObject.result);
+
+            using (DBEntities db = new DBEntities())
+            {
+                db.Slugs.Add(slugObject);
+                db.SaveChanges();
+            }
+            clearTxt();
+            getSlugsList();
+        }
+        private void getSlugsList()
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                dataGridView1.DataSource = db.Slugs.ToList<Slug>();
+            }
+        }
+
+        private void resetTextBtn_Click(object sender, EventArgs e)
+        {
+            clearTxt();
+        }
+
+        private void clearTxt()
+        {
+            widthTxt.Text = "";
+            lenghtTxt.Text = "";
+            radiousTxt.Text = "";
+            spaceTxt.Text = "";
+            labResult.Text = "";
         }
     }
 }
